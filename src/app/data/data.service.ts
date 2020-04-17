86960c228b14efe6d6051910260152aba65c21c2
Jacob Moss <jmoss@indeed.com>
2020-04-17 16:00:42 -0500

    done with httpFormPostingAndDataAccess module

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { UserSettings } from './user-settings';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  
+
+  constructor(private http: HttpClient) { }
+
+  getSubscriptionTypes(): Observable<string[]> {
+    return of(['Monthly', 'Annual', 'Lifetime']);
+  }
+
+  postUserSettingsForm(userSettings: UserSettings): Observable<any> {
+    // return of(userSettings); 
+    return this.http.post('https://putsreq.com/q06BE1rTkU40MuhiYgak', userSettings);
+  }
+}
